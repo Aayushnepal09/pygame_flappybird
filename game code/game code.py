2c14e6c21d69352cbc0dc284e1fdc4432b07be9d
Administrator <nepal3491@gmail.com>
2021-05-30 12:05:29 +0545

    10

--- game code/game code.py
+++ game code/game code.py
@@ -1,11 +1,12 @@
 import pygame  # importing pygame
 import sys  # importing sys
+import random  # importing random
 
 # initialize pygame
 pygame.init()
 
 # creating a screen
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((500, 700))
 # title and icon
 pygame.display.set_caption("FLAPPYBIRD")
 icon = pygame.image.load('redbird-downflap.png')
@@ -16,7 +17,7 @@ clock = pygame.time.Clock()
 
 # adding background image
 bg_surface = pygame.image.load('background-day.png')
-bg_surface = pygame.transform.scale(bg_surface, (500, 500))
+bg_surface = pygame.transform.scale(bg_surface, (500, 700))
 
 # adding floor image
 floor_surface = pygame.image.load('base.png')
@@ -36,17 +37,20 @@ pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1000)
+pipe_height = [500, 300, 400]
 
 
 # function to make floor continuously moving in the screen
 def floor_movement():
-    screen.blit(floor_surface, (floorX, 400))
-    screen.blit(floor_surface, (floorX + 500, 400))
+    screen.blit(floor_surface, (floorX, 600))
+    screen.blit(floor_surface, (floorX + 500, 600))
 
 
 def create_pipe():
-    pipe_new = pipe_surface.get_rect(midtop=(200, 250))
-    return pipe_new
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop=(500, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(500, random_pipe_pos - 200))
+    return bottom_pipe, top_pipe
 
 
 def move_pipes(pipes):
@@ -54,10 +58,14 @@ def move_pipes(pipes):
         pipe.centerx -= 5
     return pipes
 
+
 def draw_pipes(pipes):
     for pipe in pipes:
-        screen.blits(pipe_surface, pipe)
-
+        if pipe.bottom>=700:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe=pygame.transform.flip(pipe_surface,False,True)
+            screen.blit(flip_pipe,pipe)
 
 
 # game loop
@@ -75,9 +83,11 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
-                bird_movement -=   10
+                bird_movement -= 10
+        # adding pipe appear timer
         if event.type == SPAWNPIPE:
-            pipe_list.append(create_pipe())
+            pipe_list.extend(create_pipe())
+
     screen.blit(bg_surface, (0, 0))
 
     # bird movement
@@ -85,8 +95,8 @@ while True:
     bird_movement += fall
     bird_box.centery += bird_movement
 
-    #pipe movement
-    pipe_list=move_pipes(pipe_list)
+    # pipe movement
+    pipe_list = move_pipes(pipe_list)
     draw_pipes(pipe_list)
     # floor movement
     floorX -= 1
